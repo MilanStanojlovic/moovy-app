bcb082d136b6250f7117cb1b9ca005ab99bede76
Milan <mistikone1@gmail.com>
2019-12-14 16:57:10 +0100

    fetch movie genres from the api

--- src/containers/Genres.js
+++ src/containers/Genres.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 import Genre from '../components/Genre/Genre';
 
 class Genres extends Component {
 
-  render(){
+  state = {
+    genres: [],
+
+  }
+
+  componentDidMount() {
+    const apiKey = 'e826d7cae51a970759bd99a85655ac2f';
+    const genreListUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+
+    axios.get(genreListUrl).then(response => {
+      this.setState({genres: response.data.genres});
+      // console.log(response.data.genres);
+    })
+  }
+
+  render() {
+    const genres = this.state.genres.map(genre=> {
+      return <Genre key={genre.id} genreName={genre.name}/>
+    });
+
     return (
       <div>
         <h2>Genres</h2>
         <nav>
           <ul>
-            <Genre>Action</Genre>
-            <Genre>Comedy</Genre>
-            <Genre>Drama</Genre>
+            {genres}
           </ul>
         </nav>
       </div>
