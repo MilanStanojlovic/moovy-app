58934ccaaf19d6426851dce56d08f5332dfcf2b2
Milan <mistikone1@gmail.com>
2020-01-07 10:50:06 +0100

    implemented sidedrawer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
 import { Switch, Route } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 
 import Logo from './components/Logo/Logo';
 import NavigationItems from './components/Navigation/NavigationItems/NavigationItems';
@@ -10,35 +10,47 @@ import Genres from './components/Genres/Genres';
 import MovieList from './components/MovieList/MovieList';
 import Movie from './components/Movie/Movie';
 import Search from './components/Search/Search'
+import Backdrop from './components/Backdrop/Backdrop';
+import SideDrawerToggle from './components/SideDrawer/SideDrawerToggle/SideDrawerToggle';
+import SideDrawer from './components/SideDrawer/SideDrawer';
 
 function App() {
-  const [ShowMenu, setShowMenu] = useState(true);
+  const [ShowMenu, setShowMenu] = useState(false);
 
   const handleMenuClick = () => {
     setShowMenu(prevShowMenu => !prevShowMenu);
   }
-  let attachedSidebarStyle = [styles.Sidebar];
-  let attachedMainStyle = [styles.Main];
-  if (!ShowMenu) {
-    attachedSidebarStyle = [styles.Sidebar, styles.Invisible];
-    attachedMainStyle = [styles.Main, styles.MainSpan]
+
+  const handleBackdropClick = () => {
+    setShowMenu(false);
   }
 
+  let sideDrawer;
+  let backdrop;
+
+  if(ShowMenu){
+    sideDrawer = <SideDrawer/>;
+    backdrop = <Backdrop onBackdropClick={handleBackdropClick}/>;
+  }
 
   return (
     <div className={styles.Container}>
       <div className={styles.Logo}>
         <Logo />
-        <FontAwesomeIcon icon={faEllipsisH} onClick={handleMenuClick} size="2x" className={styles.Menu} />
+        <SideDrawerToggle onMenuClick={handleMenuClick}/>
       </div>
       <div className={styles.Top}>
         <Search />
       </div>
-      <aside className={attachedSidebarStyle.join(' ')}>
-        <NavigationItems />
-        <Genres />
+      <aside className={styles.Sidebar}>
+        <NavigationItems/>
+        <Genres/>
       </aside>
-      <main className={attachedMainStyle.join(' ')}>
+      <aside className={styles.ResponsiveSide}>
+        {sideDrawer}
+        {backdrop}
+      </aside>
+      <main className={styles.Main}>
         <Switch>
           <Route path="/" exact component={MovieList} />
           <Route path="/movie/:id" component={Movie} />
