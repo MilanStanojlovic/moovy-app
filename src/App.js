5664af745abf0b14228d062f65681a202a0057f7
Milan <mistikone1@gmail.com>
2020-01-08 10:18:08 +0100

    tmdb

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
 import { Switch, Route } from 'react-router-dom';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 
 import Logo from './components/Logo/Logo';
 import NavigationItems from './components/Navigation/NavigationItems/NavigationItems';
@@ -13,6 +11,7 @@ import Search from './components/Search/Search'
 import Backdrop from './components/Backdrop/Backdrop';
 import SideDrawerToggle from './components/SideDrawer/SideDrawerToggle/SideDrawerToggle';
 import SideDrawer from './components/SideDrawer/SideDrawer';
+import Tmdb from './components/Tmdb/Tmdb';
 
 function App() {
   const [ShowMenu, setShowMenu] = useState(false);
@@ -45,6 +44,7 @@ function App() {
       <aside className={styles.Sidebar}>
         <NavigationItems/>
         <Genres/>
+        <Tmdb/>
       </aside>
       <aside className={styles.ResponsiveSide}>
         {sideDrawer}
